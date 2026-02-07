3b4e855b0de26977fc36314ddd551a60c65f0bbd
Harsha333hhh <hv731731@gmail.com>
2026-02-07 15:04:06 +0530

    Update UserApi.js

--- week3/backend/API/UserApi.js
+++ week3/backend/API/UserApi.js
@@ -1,9 +1,14 @@
+// import express module
 import exp from "express"
+
+// import mongoose models for user and product
 import { userModel } from '../models/userModel.js'
 import { productModel } from '../models/productModel.js'
 
+// create router for user related APIs
 const userApp = exp.Router();
 
+// get all users from database
 userApp.get("/users", async (req, res) => {
     try {
         const users = await userModel.find();
@@ -14,8 +19,11 @@ userApp.get("/users", async (req, res) => {
 });
 
 export { userApp };
+
+// create router for product related APIs
 const productApp = exp.Router();
 
+// get all products from database
 productApp.get("/product", async (req, res) => {
     try {
         const products = await productModel.find();
@@ -27,78 +35,112 @@ productApp.get("/product", async (req, res) => {
 
 export { productApp };
 
-// user api routes
 
-//create user 
-userApp.post('/users',async(req,res)=>{
-    let newUser=req.body
-    let newuserDoc=new userModel(newUser)
-    await newuserDoc.save()
-    res.status(201).json({message:"user created",payload:newuserDoc})
-})
-// read all users 
-userApp.get('/users',async(req,res)=>{})
+// -------- USER API ROUTES --------
 
+// create new user
+userApp.post('/users', async (req, res) => {
 
-// read users by objjectID
-userApp.get("/users/:id",async(req,res)=>{
-    // get ibj id from url param
-    let objId=req.params.id;
-    //find user in db
-    let userObj=await userModel.findById(objId)
-    // send res
-    res.status(200).json({message:"user",payload:userObj})
+    // get user data from request body
+    let newUser = req.body;
 
-})
+    // create mongoose document
+    let newuserDoc = new userModel(newUser);
 
-// update user 
+    // save user to database
+    await newuserDoc.save();
 
-userApp.put("/users/:id",async(req,res)=>{
-
-    // GEt objecy id from params
-    let objId=req.params.id;
-    //get modified user from req
-    let modifieduser=req.body;
-    //make update
-    let latestUser=await userModel.findByIdAndUpdate(objId,{$set:{...modifieduser}},{new:true,runValidators:true})
-    // send res
-    res.status(200).json({message:"user modiofied",payload:latestUser})
-
+    res.status(201).json({ message: "user created", payload: newuserDoc });
 });
+
+// placeholder route to read all users (not implemented)
+userApp.get('/users', async (req, res) => { });
+
+
+// read user by object id
+userApp.get("/users/:id", async (req, res) => {
+
+    // get object id from url parameter
+    let objId = req.params.id;
+
+    // find user in database by id
+    let userObj = await userModel.findById(objId);
+
+    // send user as response
+    res.status(200).json({ message: "user", payload: userObj });
+});
+
+
+// update user by id
+userApp.put("/users/:id", async (req, res) => {
+
+    // get object id from params
+    let objId = req.params.id;
+
+    // get modified user data from request body
+    let modifieduser = req.body;
+
+    // update user and return latest document
+    let latestUser = await userModel.findByIdAndUpdate(
+        objId,
+        { $set: { ...modifieduser } },
+        { new: true, runValidators: true }
+    );
+
+    res.status(200).json({ message: "user modiofied", payload: latestUser });
+});
+
 
 // delete user by id
+userApp.delete("/users/:id", async (req, res) => {
 
-userApp.delete("/users/:id",async(req,res)=>{
-// get obj id
-let objId=req.params.id;
-//delete user by id 
-let deleteduser=await userModel.findByIdAndDelete(objId);
-res.status(200).json({message:"user removed",payload:deleteduser});
+    // get object id from params
+    let objId = req.params.id;
 
+    // delete user from database
+    let deleteduser = await userModel.findByIdAndDelete(objId);
+
+    res.status(200).json({ message: "user removed", payload: deleteduser });
 });
 
 
-// create product
-productApp.post("/product",async(req,res)=>{
-    let newProduct=req.body;
-    let newProductsDoc=new productModel(newProduct);
+// -------- PRODUCT API ROUTES --------
+
+// create new product
+productApp.post("/product", async (req, res) => {
+
+    // get product data from body
+    let newProduct = req.body;
+
+    // create mongoose document
+    let newProductsDoc = new productModel(newProduct);
+
+    // save product to database
     await newProductsDoc.save();
-    res.status(201).json({message:"product added",payload:newProductsDoc});
-})
 
-// find product by id 
+    res.status(201).json({ message: "product added", payload: newProductsDoc });
+});
 
-productApp.get("/product/:id",async(req,res)=>{
-    let productId=req.params.id;
-    let productObj=await productModel.findById(productId);
-    res.status(200).json({message:"product found",payload:productObj});
+// find product by id
+productApp.get("/product/:id", async (req, res) => {
 
-})
+    // get product id from url
+    let productId = req.params.id;
 
-// delete product
+    // find product in database
+    let productObj = await productModel.findById(productId);
 
-productApp.delete("/product/:id",async(req,res)=>{
-    let productId=req.params.id;
-    let deletedProduct=await productModel.findByIdAndDelete(productId);
-    res.status(200).json({message:"the product is deleted",payload:deletedProduct});
-})
+    res.status(200).json({ message: "product found", payload: productObj });
+});
+
+// delete product by id
+productApp.delete("/product/:id", async (req, res) => {
+
+    // get product id from url
+    let productId = req.params.id;
+
+    // delete product from database
+    let deletedProduct = await productModel.findByIdAndDelete(productId);
+
+    res.status(200).json({ message: "the product is deleted", payload: deletedProduct });
+});
