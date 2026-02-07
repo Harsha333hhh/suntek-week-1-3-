99cf74c7c73a5fdcc3a7a1c881ceec02d3c4c61e
Harsha333hhh <hv731731@gmail.com>
2026-02-07 14:49:52 +0530

    Refactor studentMarks.js with comments and clarity
    Refactored code for clarity and added comments for better understanding.

--- week1/day1.js/studentMarks.js
+++ week1/day1.js/studentMarks.js
@@ -1,23 +1,34 @@
-let marks=[10,20,30];
-let skills=["html","css","javaScript"]; // student skills
+let marks = [10, 20, 30];
+let skills = ["html", "css", "javaScript"]; // student skills
+
+// accessing index 20 which does not exist will give undefined
 console.log(marks[20]);
-for(let i=0;i<marks.length;i++){ // retriving marks
-    console.log(marks[i]);
-    console.log(i)
+
+// loop to print all marks with their index
+for(let i = 0; i < marks.length; i++){ 
+    console.log(marks[i]);   // print mark value
+    console.log(i);          // print index
 }
-let s=0;
-let min=100;
+
+let s = 0;
+let min = 100;
+
+// function to calculate sum and minimum mark
 function dec(marks){
-for(let i of marks){  // using for loop 
-    s+=i;
-    console.log(i)
-    if (i<min){
-        min=i;
-    }
+  for(let i of marks){      // iterate through each mark
+      s += i;               // add mark to sum
+      console.log(i);
 
-}
-console.log("min is "+min);
-console.log("sum is "+s);
+      // check for minimum value
+      if (i < min){
+          min = i;
+      }
+  }
+
+  console.log("min is " + min);
+  console.log("sum is " + s);
 }
 
+// call function with marks array
 dec(marks);
+
