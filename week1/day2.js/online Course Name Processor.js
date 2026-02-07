b3e25a19f406adf394b332d7ee7946b642c1b862
Harsha333hhh <hv731731@gmail.com>
2026-02-07 14:45:20 +0530

    Refactor online Course Name Processor.js functions

--- week1/day2.js/online Course Name Processor.js
+++ week1/day2.js/online Course Name Processor.js
@@ -1,23 +1,32 @@
-const cor= ["javascript", "react", "node", "mongodb", "express"];
-let len=cor.filter(function(i){
-    return i.length>5;
+// array containing course names
+const cor = ["javascript", "react", "node", "mongodb", "express"];
+
+// filter courses whose length is greater than 5 characters
+let len = cor.filter(function(i){
+    return i.length > 5;
 });
-console.log("course names with length greater than 5 are: ",len);
-let up=cor.map(function(i){
+console.log("course names with length greater than 5 are: ", len);
+
+// convert all course names to uppercase using map
+let up = cor.map(function(i){
     return i.toUpperCase();
-})
-console.log("course names in uppercase are: ",up);
-let sing=cor.reduce(function(acc,rel){
-    return acc+","+rel;
+});
+console.log("course names in uppercase are: ", up);
 
+// join all course names into a single comma separated string
+let sing = cor.reduce(function(acc, rel){
+    return acc + "," + rel;
 });
-console.log("course names in a single string are: ",sing);
-let fin=cor.find(function(i){
-    return i==="react";
-});
-console.log("course name equal to react is: ",fin);
+console.log("course names in a single string are: ", sing);
 
-let finIn=cor.findIndex(function(i){
-    return i==="node";
+// find the course name that exactly matches "react"
+let fin = cor.find(function(i){
+    return i === "react";
 });
-console.log("index of course name equal to node is: ",finIn);
+console.log("course name equal to react is: ", fin);
+
+// find index position of course name "node"
+let finIn = cor.findIndex(function(i){
+    return i === "node";
+});
+console.log("index of course name equal to node is: ", finIn);
