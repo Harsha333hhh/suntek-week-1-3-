14826559816650eb88b2aaff0c2ca085ef99feaa
Harsha333hhh <hv731731@gmail.com>
2026-02-07 14:27:14 +0530

    Update and rename 3.js to studentMarks.js

--- week1/day1.js/studentMarks.js
+++ week1/day1.js/studentMarks.js
@@ -1,14 +1,14 @@
 let marks=[10,20,30];
-let skills=["html","css","javaScript"];
+let skills=["html","css","javaScript"]; // student skills
 console.log(marks[20]);
-for(let i=0;i<marks.length;i++){
+for(let i=0;i<marks.length;i++){ // retriving marks
     console.log(marks[i]);
     console.log(i)
 }
 let s=0;
 let min=100;
 function dec(marks){
-for(let i of marks){
+for(let i of marks){  // using for loop 
     s+=i;
     console.log(i)
     if (i<min){
@@ -19,4 +19,5 @@ for(let i of marks){
 console.log("min is "+min);
 console.log("sum is "+s);
 }
+
 dec(marks);
