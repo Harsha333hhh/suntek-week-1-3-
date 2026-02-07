52a4dbd257388171d4bdfd5514e52a99ea884999
Harsha333hhh <hv731731@gmail.com>
2026-02-07 14:25:27 +0530

    Update and rename 1.js to functionTemp.js

--- week1/day1.js/functionTemp.js
+++ week1/day1.js/functionTemp.js
@@ -2,9 +2,9 @@ console.log('This is file 1.js');
 console.log('Hello from 1.js!');
 console.log('1.js is executing some code.');
 let username="JohnDoe";
-let marks=[95, 90, 98];
+let marks=[95, 90, 98]; // arrsy
 let status=true;
-let student={
+let student={  //object
     sno:20,
     sname:"Alice"
 }
@@ -14,7 +14,7 @@ console.log(typeof b);
 console.log(typeof username); 
 console.log(typeof marks);
 console.log(typeof status); 
-function greet(d,f){
+function greet(d,f){  // this is a function
     console.log('this is a function from 1.js');
     console.log(`Sum is: ${d+f}`);
 }
@@ -31,4 +31,5 @@ function greatest(x,y,z){
         console.log("c is greatest");
     }
 }
+
 greatest(10,20,15);
