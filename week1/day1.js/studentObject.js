29110956f17c10b50207e064a3808d24d0f66d77
Harsha333hhh <hv731731@gmail.com>
2026-02-07 14:36:32 +0530

    Update and rename task6.js to studentObject.js

--- week1/day1.js/studentObject.js
+++ week1/day1.js/studentObject.js
@@ -1,11 +1,12 @@
-let Student={
+let Student={  // student object 
     roll:100,
     name:"alex",
     age:20,
     city:"hyd"
 }
-for(let i in Student){
+for(let i in Student){  // using for loop to retrivig the data 
     console.log(i);
     console.log("i is "+Student[i]);
     console.log(i,"is ",+Student[i]);
+
 }
