e254dd7b1a9e4511556183a8e52aa1b6cf35c607
Harsha333hhh <hv731731@gmail.com>
2026-02-07 14:46:46 +0530

    Update task2.js'.js

--- week1/day2.js/task2.js'.js
+++ week1/day2.js/task2.js'.js
@@ -1,22 +1,34 @@
+// object storing subject wise marks
 const marks = {
   maths: 78,
   physics: 65,
   chemistry: 82,
   english: 55
 }
+
 let total = 0;
-let markCount = 0;
+
+// loop through object to calculate total marks
 for(let i in marks){
-  total+=marks[i];
+  total += marks[i];
 }
-let great=0;
+
+let great = 0;
+
 console.log("Total Marks: " + total);
-console.log("Average Marks: " + (total/Object.keys(marks).length));
+
+// calculate average using total subjects count from object keys
+console.log("Average Marks: " + (total / Object.keys(marks).length));
+
+// adding a new subject and marks dynamically
 marks.computer = 90;
 console.log(marks);
+
+// find the greatest marks among all subjects
 for(let i in marks){
-    if(marks[i]>great){
-        great=marks[i];
-    }
+  if(marks[i] > great){
+      great = marks[i];
+  }
 }
-    console.log("greatest marks is :"+great);
+
+console.log("greatest marks is :" + great);
